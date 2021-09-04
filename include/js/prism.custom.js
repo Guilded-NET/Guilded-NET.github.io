bcf8ed9ffd735a10a50865c4f54a543a6b766912
IdkGoodName <31628630+IdkGoodName@users.noreply.github.com>
2021-09-04 20:27:48 +0300

    Add more data to codeblocks; fix lines above 9 pushing the text

--- include/js/prism.custom.js
+++ include/js/prism.custom.js
@@ -1,6 +1,13 @@
 // The list was taken from Prism.js language highlight/toolbar plugin
 const langNames = {none:"Plain text",plain:"Plain text",plaintext:"Plain text",text:"Plain text",txt:"Plain text",html:"HTML",xml:"XML",svg:"SVG",mathml:"MathML",css:"CSS",clike:"C-like",js:"JavaScript",asciidoc:"AsciiDoc",adoc:"AsciiDoc",aspnet:"ASP.NET (C#)",csharp:"C#",cs:"C#",dotnet:"C#",cpp:"C++",cil:"CIL","css-extras":"CSS Extras",dockerfile:"Docker",editorconfig:"EditorConfig",fsharp:"F#",http:"HTTP",ignore:".ignore",gitignore:".gitignore",hgignore:".hgignore",javadoclike:"JavaDoc-like",javastacktrace:"Java stack trace",jsdoc:"JSDoc","js-extras":"JS Extras",json:"JSON",json5:"JSON5",jsonp:"JSONP",jsstacktrace:"JS stack trace","js-templates":"JS Templates",latex:"LaTeX",tex:"TeX",context:"ConTeXt",log:"Log file",md:"Markdown","markup-templating":"Markup templating",mongodb:"MongoDB",plsql:"PL/SQL",powerquery:"PowerQuery",pq:"PowerQuery",mscript:"PowerQuery",powershell:"PowerShell",properties:".properties",qsharp:"Q#",qs:"Q#",q:"Q (kdb+ database)",qml:"QML",jsx:"React JSX",tsx:"React TSX","shell-session":"Shell session","sh-session":"Shell session",shellsession:"Shell session","solution-file":"Solution file",sln:"Solution file",sql:"SQL",iecst:"Structured Text (IEC 61131-3)","t4-templating":"T4 templating","t4-cs":"T4 Text Templates (C#)",t4:"T4 Text Templates (C#)","t4-vb":"T4 Text Templates (VB)",tt2:"Template Toolkit 2",ts:"TypeScript",tsconfig:"TSConfig",vbnet:"VB.Net","visual-basic":"Visual Basic",vba:"VBA",vb:"Visual Basic","xml-doc":"XML doc (.net)"}
 
+// Gets attribute value as array
+function getArrayAttribute(elem, attr) {
+    return elem.attributes[attr] ? elem.attributes[attr].value.split(',') : []
+}
+
+// TODO: Fix some mess
+
 // Add custom lines, not the weird ones at the end of the code
 Prism.hooks.add('linenums', e => {
     const elem = e.element
@@ -13,7 +20,9 @@ Prism.hooks.add('linenums', e => {
     // Split the contents by newlines
     const lines = inner.split('\n')
     // Get highlighted line
-    const highlighted = elem.attributes['data-highlight'] ? elem.attributes['data-highlight'].value.split(',') : []
+    const highlighted = getArrayAttribute(elem, 'data-highlight'),
+          inserted    = getArrayAttribute(elem, 'data-insert'),
+          deleted     = getArrayAttribute(elem, 'data-delete')
     // Get each line in the contents
     for(let i in lines) {
         const line = lines[i]
@@ -21,8 +30,13 @@ Prism.hooks.add('linenums', e => {
         const lineElem = document.createElement('codeline')
         lineElem.innerHTML = line
         // Check if it's highlighted
+        // Ol' and boring code
         if(highlighted.includes(i))
             lineElem.classList.add('highlighted')
+        else if(inserted.includes(i))
+            lineElem.classList.add('inserted')
+        else if(deleted.includes(i))
+            lineElem.classList.add('deleted')
         // Append it to the code block
         elem.appendChild(lineElem)
     }
@@ -42,6 +56,9 @@ Prism.hooks.add('codewrapper', e => {
     lang.innerHTML = langNames[e.language]
     // Add everything to the wrapper
     wrapper.prepend(lang)
+    // Wrap around contents
+    const content = document.createElement('div')
+    content.classList.add('code-content')
     // Header above the code block
     const header = document.createElement('div')
     header.classList.add('code-header')
@@ -54,11 +71,9 @@ Prism.hooks.add('codewrapper', e => {
         file.innerHTML = `<span class="code-data-label">File:</span> <span class="code-data-value">${filename}</span>`
         // And append it
         header.appendChild(file)
+        // Only add a header if filename is specified
+        content.appendChild(header)
     }
-    // Wrap around contents
-    const content = document.createElement('div')
-    content.classList.add('code-content')
-    content.appendChild(header)
     wrapper.appendChild(content)
     // Wrap around the code
     base.parentElement.insertBefore(wrapper, base)
