aca56b667d9b2f71e64e00ab08fc3cb5fd5d6916
IdkGoodName <31628630+IdkGoodName@users.noreply.github.com>
2021-09-24 18:39:50 +0300

    Make code blocks with no language have no label

--- include/js/prism.custom.js
+++ include/js/prism.custom.js
@@ -4,6 +4,8 @@
 // Modified: Removed languages that will be guaranteed to never be used
 const langNames = { none: "Plain text", plain: "Plain text", plaintext: "Plain text", text: "Plain text", txt: "Plain text", html: "HTML", xml: "XML", svg: "SVG", mathml: "MathML", css: "CSS", clike: "C-like", js: "JavaScript", asciidoc: "AsciiDoc", adoc: "AsciiDoc", aspnet: "ASP.NET (C#)", bash: 'bash', csharp: "C#", cs: "C#", dotnet: "C#", cpp: "C++", cil: "CIL", "css-extras": "CSS Extras", dockerfile: "Docker", editorconfig: "EditorConfig", fsharp: "F#", http: "HTTP", ignore: ".ignore", gitignore: ".gitignore", hgignore: ".hgignore", javadoclike: "JavaDoc-like", javastacktrace: "Java stack trace", jsdoc: "JSDoc", "js-extras": "JS Extras", json: "JSON", json5: "JSON5", jsonp: "JSONP", jsstacktrace: "JS stack trace", "js-templates": "JS Templates", latex: "LaTeX", tex: "TeX", context: "ConTeXt", log: "Log file", md: "Markdown", "markup-templating": "Markup templating", mongodb: "MongoDB", plsql: "PL/SQL", powerquery: "PowerQuery", pq: "PowerQuery", mscript: "PowerQuery", powershell: "PowerShell", properties: ".properties", qsharp: "Q#", qs: "Q#", q: "Q (kdb+ database)", qml: "QML", jsx: "React JSX", tsx: "React TSX", "shell-session": "Shell session", "sh-session": "Shell session", shellsession: "Shell session", "solution-file": "Solution file", sln: "Solution file", sql: "SQL", iecst: "Structured Text (IEC 61131-3)", "t4-templating": "T4 templating", "t4-cs": "T4 Text Templates (C#)", t4: "T4 Text Templates (C#)", "t4-vb": "T4 Text Templates (VB)", tt2: "Template Toolkit 2", ts: "TypeScript", tsconfig: "TSConfig", vbnet: "VB.Net", "visual-basic": "Visual Basic", vba: "VBA", vb: "Visual Basic", "xml-doc": "XML doc (.net)" }
 
+const namelessLangs = [ 'none', 'plain', 'plaintext', 'text', 'txt', undefined ]
+
 
 function getAttribute(elem, attr) {
     const attrValue = (elem.attributes[attr] || elem.parentElement.attributes[attr])
@@ -65,12 +67,15 @@ Prism.hooks.add('codewrapper', e => {
     const base = elem.parentElement
 
     const wrapper = document.createElement('codeblock')
-    // Lang name display
-    const lang = document.createElement('span')
-    lang.classList.add('code-lang')
-    lang.innerHTML = langNames[e.language]
 
-    wrapper.prepend(lang)
+    if(!namelessLangs.includes(e.language)) {
+        // Lang name display
+        const lang = document.createElement('span')
+        lang.classList.add('code-lang')
+        lang.innerHTML = langNames[e.language]
+    
+        wrapper.prepend(lang)
+    }
 
     // Content wrapper
     const content = document.createElement('div')
